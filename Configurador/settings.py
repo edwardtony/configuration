d9622446e5ddeb72968b71eca9125e42b7d13e01
Anthony Del Pozo <delan1997@gmail.com>
2017-11-28 21:42:08 -0500

    DEBUG = FALSE

--- Configurador/settings.py
+++ Configurador/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y(pqpsi-@_5lbsaqx7j15hia@*d&1jbf5eu!&dx52yk-4f24c$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
